6f3a6fb446451f9e6976fd9ee76de833cca151a4
damien <damiengabrielle@gmail.com>
2021-10-02 13:09:17 +0200

    better cursor position

--- test/Level.gd
+++ test/Level.gd
@@ -1,11 +1,13 @@
 extends Spatial
 
 onready var Level = preload("res://test/Block.tscn")
-onready var r0 : RayCast = $R0
+onready var ray_ground : RayCast = $RayGround
 onready var ray : RayCast = $RayCast
-onready var pointer : MeshInstance = $Pointer
+onready var pointer : MeshInstance = $Debug/Pointer
 onready var camera : OrbitalCamera = $OrbitalCamera
 var m_position : Vector2 = Vector2.ZERO
+var _target : Vector3 = Vector3.ZERO
+const CRANE_SPEED : float = 4.0
 
 func _ready() -> void:
 	pass
@@ -18,16 +20,33 @@ func _input(event: InputEvent) -> void:
 		add_child(block)
 		block.global_transform.origin = pointer.global_transform.origin + Vector3(0, 3, 0)
 
+func _process(_delta: float) -> void:
+	pass
+
 func _physics_process(delta: float) -> void:
 	if not camera.dragging:
 		update_target_position()
 		var from = camera.project_ray_origin(m_position)
-		r0.global_transform.origin = from
-		r0.cast_to = from + camera.project_ray_normal(m_position) * 100
-		if r0.is_colliding():
-			ray.global_transform.origin = r0.get_collision_point() + Vector3(0, 8, 0)
-			if ray.is_colliding():
-				pointer.global_transform.origin = ray.get_collision_point()
+		ray_ground.global_transform.origin = from
+		ray_ground.cast_to = from + camera.project_ray_normal(m_position) * 100
+		if ray_ground.is_colliding():
+			_target = ray_ground.get_collision_point()
+			$Debug/Target.global_transform.origin = _target
+		
+	var p0 = ray.global_transform.origin
+	var p = lerp(Vector3(p0.x, 8, p0.z), Vector3(_target.x, 8, _target.z), delta * CRANE_SPEED)
+	ray.global_transform.origin = p
+	if ray.is_colliding():
+		pointer.global_transform.origin = ray.get_collision_point()
 
 func update_target_position() -> void:
 	pass
+
+
+func _on_OrbitalCamera_orbiting_end(mouse_position: Vector2) -> void:
+	var _end_drag : Vector2 = camera.unproject_position(_target)
+	camera.set_delta_drag(mouse_position - _end_drag)
+
+func _on_OrbitalCamera_orbiting_start() -> void:
+	pass
+
--- test/OrbitalCamera.gd
+++ test/OrbitalCamera.gd
@@ -1,12 +1,16 @@
 class_name OrbitalCamera
 extends Spatial
 
+signal orbiting_start()
+signal orbiting_end(position)
+
 onready var camera : Camera = $Camera
 var dragging : bool = false
 
 var _target_angle : float = 0.0
 var _initial_mouse_position : Vector2 = Vector2.ZERO
 var _initial_basis : Basis = Basis()
+var _drag_delta : Vector2 = Vector2.ZERO
 
 const ORBITAL_ROTATION : float = 1.5 * PI
 
@@ -15,25 +19,39 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("camera-drag") and not dragging:
-		dragging = true
-		_initial_mouse_position = event.position
-		_initial_basis = global_transform.basis
-		_target_angle = 0.0
+		_start_dragging(event.position)
 
 	if event.is_action_released("camera-drag"):
-		dragging = false
+		_end_dragging(event.position)
 
 	if event is InputEventMouseMotion:
-		var delta_p = _initial_mouse_position - event.position
+		var delta_p = _initial_mouse_position - (event.position)
 		var vp = get_viewport().get_visible_rect().size / 2
 		_target_angle = (delta_p.x / vp.x) * ORBITAL_ROTATION
 
-func _process(delta: float) -> void:
+func _start_dragging(event_position: Vector2) -> void:
+	dragging = true
+	_initial_mouse_position = event_position
+	_initial_basis = global_transform.basis
+	_target_angle = 0.0
+	emit_signal("orbiting_start")
+
+func _end_dragging(event_position: Vector2) -> void:
+	dragging = false
+	emit_signal("orbiting_end", event_position)
+
+func _process(_delta: float) -> void:
 	if dragging:
 		global_transform.basis = _initial_basis.rotated(Vector3.UP, _target_angle)
 
+func set_delta_drag(delta: Vector2) -> void:
+	_drag_delta = delta
+
 func project_ray_origin(position: Vector2) -> Vector3:
-	return camera.project_ray_origin(position)
+	return camera.project_ray_origin(position - _drag_delta)
 
 func project_ray_normal(position: Vector2) -> Vector3:
-	return camera.project_ray_normal(position)
+	return camera.project_ray_normal(position - _drag_delta)
+
+func unproject_position(position: Vector3) -> Vector2:
+	return camera.unproject_position(position)
