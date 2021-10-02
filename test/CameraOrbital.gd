3f5cca07941309d897cbe80637218b00cbfcd5ac
damien <damiengabrielle@gmail.com>
2021-10-02 10:10:03 +0200

    orbital camera

---
+++ test/CameraOrbital.gd
@@ -0,0 +1,33 @@
+class_name CameraOrbital
+extends Spatial
+
+onready var camera : Camera = $Camera
+var dragging : bool = false
+
+var _target_angle : float = 0.0
+var _initial_mouse_position : Vector2 = Vector2.ZERO
+var _initial_basis : Basis = Basis()
+
+const ORBITAL_ROTATION : float = 1.5 * PI
+
+func _ready() -> void:
+	camera.look_at(global_transform.origin, Vector3.UP)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("camera-drag") and not dragging:
+		dragging = true
+		_initial_mouse_position = event.position
+		_initial_basis = global_transform.basis
+		_target_angle = 0.0
+
+	if event.is_action_released("camera-drag"):
+		dragging = false
+
+	if event is InputEventMouseMotion:
+		var delta_p = _initial_mouse_position - event.position
+		var vp = get_viewport().get_visible_rect().size / 2
+		_target_angle = (delta_p.x / vp.x) * ORBITAL_ROTATION
+
+func _process(delta: float) -> void:
+	if dragging:
+		global_transform.basis = _initial_basis.rotated(Vector3.UP, _target_angle)
--- test/Level.gd
+++ test/Level.gd
@@ -4,11 +4,11 @@ onready var Level = preload("res://test/Block.tscn")
 onready var r0 : RayCast = $R0
 onready var ray : RayCast = $RayCast
 onready var pointer : MeshInstance = $Pointer
-onready var camera : Camera = $CameraPivot/Camera
+#onready var camera : Camera = $CameraPivot/Camera
 var m_position : Vector2 = Vector2.ZERO
 
 func _ready() -> void:
-	camera.look_at($CameraPivot.global_transform.origin, Vector3.UP)
+	pass
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -18,13 +18,13 @@ func _input(event: InputEvent) -> void:
 		add_child(block)
 		block.global_transform.origin = pointer.global_transform.origin + Vector3(0, 3, 0)
 
-func _physics_process(delta: float) -> void:
-	var from = camera.project_ray_origin(m_position)
-	var to = from + camera.project_ray_normal(m_position) * 100
-	r0.global_transform.origin = from
-	r0.cast_to = to
-	if r0.is_colliding():
-		ray.global_transform.origin = r0.get_collision_point() + Vector3(0, 8, 0)
-		if ray.is_colliding():
-			pointer.global_transform.origin = ray.get_collision_point()
+#func _physics_process(delta: float) -> void:
+#	var from = camera.project_ray_origin(m_position)
+#	var to = from + camera.project_ray_normal(m_position) * 100
+#	r0.global_transform.origin = from
+#	r0.cast_to = to
+#	if r0.is_colliding():
+#		ray.global_transform.origin = r0.get_collision_point() + Vector3(0, 8, 0)
+#		if ray.is_colliding():
+#			pointer.global_transform.origin = ray.get_collision_point()
 
