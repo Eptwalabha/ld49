70c7ef2132092cc59b5fc5f300df2053c776b198
damien <damiengabrielle@gmail.com>
2021-10-02 14:45:42 +0200

    improve crane

--- test/Crane.gd
+++ test/Crane.gd
@@ -2,7 +2,7 @@ class_name Crane
 extends Spatial
 
 onready var arm_pivot : Spatial = $Pivot
-onready var chariot : Spatial = $Pivot/Cabin/Arm/Chariot
+onready var chariot : Spatial = $Pivot/Chariot
 
 func point_at(position: Vector3) -> void:
 	var p2 = position
@@ -11,4 +11,5 @@ func point_at(position: Vector3) -> void:
 	var y = arm_pivot.global_transform.origin.y
 	arm_pivot.look_at(Vector3(x, y, z), Vector3.UP)
 	var dist = to_local(Vector3(x, 0, z)).length()
-	chariot.transform.origin = Vector3(dist - 3.5, 0, 0)
+	dist = min(8, max(1, dist))
+	chariot.transform.origin = Vector3(0, 0, -dist)
