9870f02586dc10387bd2734f810470b83dfba5af
damien <damiengabrielle@gmail.com>
2021-10-04 23:48:45 +0200

    fix splash screen warning

--- test/splash/SplashScreen.gd
+++ test/splash/SplashScreen.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	$AnimationPlayer.play("fade-in")
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("context_action") or Input.is_action_just_pressed("ui_cancel"):
+	if event is InputEventMouseButton or Input.is_action_just_pressed("ui_cancel"):
 		load_game()
 
 func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
